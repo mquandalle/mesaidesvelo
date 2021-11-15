1de28756a1168bc7cdcf05b6cfa0cb1a61cbf919
Maxime Quandalle <maxime@quandalle.com>
2021-11-15 18:52:21 +0100

    Ajout d'un sitemap

--- src/routes/api/collectivites.js
+++ src/routes/api/collectivites.js
@@ -14,6 +14,7 @@
 import fuzzysort from 'fuzzysort';
 import communes from '@etalab/decoupage-administratif/data/communes.json';
 import epci from '@etalab/decoupage-administratif/data/epci.json';
+import { slugify } from '$lib/utils';
 
 const communesInEpci = epci.reduce((acc, { nom, membres }) => {
 	return { ...acc, ...Object.fromEntries(membres.map(({ code }) => [code, nom])) };
@@ -77,31 +78,4 @@ export async function get({ query }) {
 				.map(pick)
 		};
 	}
-}
-
-// First Google Search result, maybe there are better functions
-function slugify(str) {
-	str = str.replace(/^\s+|\s+$/g, '');
-
-	// Make the string lowercase
-	str = str.toLowerCase();
-
-	// Remove accents, swap ñ for n, etc
-	var from =
-		'ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;';
-	var to =
-		'AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------';
-	for (var i = 0, l = from.length; i < l; i++) {
-		str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-	}
-
-	// Remove invalid chars
-	str = str
-		.replace(/[^a-z0-9 -]/g, '')
-		// Collapse whitespace and replace by -
-		.replace(/\s+/g, '-')
-		// Collapse dashes
-		.replace(/-+/g, '-');
-
-	return str;
 }
---
+++ src/routes/sitemap.txt.js
@@ -0,0 +1,24 @@
+// Un sitemap avec les 50 villes les plus peuplées
+// Peu utile tant que l'on a pas de SSR
+import communes from '@etalab/decoupage-administratif/data/communes.json';
+import { slugify } from '$lib/utils';
+
+const data = communes.filter(
+	(c) =>
+		c.type === 'commune-actuelle' && c.codesPostaux && c.population && !c.code?.startsWith('97')
+);
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function get({}) {
+	return {
+		body: [
+			'/',
+			...data
+				.sort((a, b) => b.population - a.population)
+				.slice(0, 50)
+				.map(({ nom }) => `/ville/${slugify(nom)}`)
+		]
+			.map((path) => `https://mesaidesvelo.fr${path}`)
+			.join('\n')
+	};
+}
