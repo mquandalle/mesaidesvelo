6023dc98243ac667bc569a03a8dde54aeee39eef
Maxime Quandalle <maxime@quandalle.com>
2021-11-15 19:46:46 +0100

    Améliore le design du champ de recherche
    - Suppression de l'auto-fill sur Chrome
    - Selection du contenu au clic
    - Retouches visuelles
    - Annule les précédentes requêtes HTTP en cas de saisie rapide avant
      affichage des suggestions

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -4,18 +4,27 @@
 	import { localisation } from '$lib/stores/localisation';
 	import AutoComplete from 'simple-svelte-autocomplete';
 
+	// cancellebale fetch
+	let previousFetchAbortController = null;
 	async function loadItems(keyword) {
+		previousFetchAbortController?.abort();
+		previousFetchAbortController = new AbortController();
 		const url = `/api/collectivites?search=${encodeURIComponent(keyword)}`;
-		const response = await fetch(url);
+		const response = await fetch(url, { signal: previousFetchAbortController.signal });
 		return await response.json();
+	}
+
+	function autoSelectInput() {
+		const inputElement = document.getElementById('localisation-input');
+		inputElement.select();
 	}
 </script>
 
 <div
-	class="bg-green-100 p-6 mt-6 flex flex-col gap-x-4 gap-y-2 sm:flex-row sm:items-center rounded-md"
+	class="bg-green-100 py-8 px-4 sm:px-6 mt-6 flex flex-col gap-x-4 gap-y-2 sm:flex-row sm:items-center rounded"
 >
-	<label for="localisation">Localisation :</label>
-	<div class="flex-1 flex flex-row items-center bg-white shadow rounded px-4 py-1">
+	<label for="localisation-input">Localisation :</label>
+	<div class="flex-1 flex flex-row items-center bg-white shadow rounded-md pl-3 pt-1">
 		<svg
 			focusable="false"
 			xmlns="http://www.w3.org/2000/svg"
@@ -25,26 +34,32 @@
 				d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
 			/></svg
 		>
-		<AutoComplete
-			className="flex-1"
-			labelFunction={({ codePostal, nom }) => codePostal + ' - ' + nom}
-			placeholder="Commune ou Code postal"
-			searchFunction={loadItems}
-			localFiltering={false}
-			hideArrow={true}
-			showClear={!!$localisation}
-			inputId="localisation"
-			bind:selectedItem={$localisation}
-			onChange={(val) => {
-				if (typeof window !== 'undefined') {
-					if (val) {
-						goto(`/ville/${val.slug}`, { noscroll: true });
-					} else {
-						goto('/', { noscroll: true });
+		<form class="flex-1" autocomplete="off" on:submit|preventDefault>
+			<AutoComplete
+				className="w-full"
+				labelFunction={({ codePostal, nom }) => codePostal + ' - ' + nom}
+				placeholder="Commune ou Code postal"
+				searchFunction={loadItems}
+				localFiltering={false}
+				hideArrow={true}
+				showClear={!!$localisation}
+				inputId="localisation-input"
+				bind:selectedItem={$localisation}
+				onFocus={autoSelectInput}
+				onChange={(val) => {
+					if (typeof window !== 'undefined') {
+						if (val) {
+							goto(`/ville/${val.slug}`, { noscroll: true });
+						} else {
+							goto('/', { noscroll: true });
+						}
 					}
-				}
-			}}
-		/>
+				}}
+			>
+				<div slot="loading">Chargement...</div>
+				<div slot="no-results">Pas de résultats</div></AutoComplete
+			>
+		</form>
 	</div>
 </div>
 
@@ -57,7 +72,8 @@
 		background-color: #10b981 !important;
 	}
 
-	:global(#localisation) {
+	:global(#localisation-input) {
 		outline: transparent;
+		height: 34px;
 	}
 </style>
