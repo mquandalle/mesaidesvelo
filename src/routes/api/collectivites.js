246a616949215bc246bc4c5e60e384553a072fbd
Maxime Quandalle <maxime@mesaidesvelo.fr>
2021-12-17 09:25:37 +0100

    Réduction de la latence pour la recherche
    En commençant à construire l'index dès que la page d'accueil est appelée
    sans attendre que l'utilisateur commence une recherche.

--- src/routes/api/collectivites.js
+++ src/routes/api/collectivites.js
@@ -13,22 +13,7 @@
 
 import fuzzysort from 'fuzzysort';
 import { removeAccents } from '$lib/utils';
-import data from '../../data/communes.json';
-
-const indexedData = data
-	.flatMap(({ codesPostaux, ...rest }) =>
-		(codesPostaux ?? [rest.codePostal]).map((cp, i) => ({
-			...rest,
-			codePostal: cp,
-			cpPrincipal: i === 0,
-			population: i === 0 ? rest.population : 10
-		}))
-	)
-	.map((c) => ({
-		...c,
-		indexedName: c.cpPrincipal ? fuzzysort.prepare(removeAccents(c.nom)) : '',
-		indexedCodePostal: fuzzysort.prepare(c.codePostal)
-	}));
+import data from '$lib/indexed-data-communes';
 
 const searchOptions = {
 	keys: ['indexedName', 'indexedCodePostal'],
@@ -64,12 +49,12 @@ export async function get({ query }) {
 		}
 	} else if (search) {
 		return {
-			body: fuzzysort.go(search, indexedData, searchOptions).map(({ obj }) => pick(obj))
+			body: fuzzysort.go(search, data, searchOptions).map(({ obj }) => pick(obj))
 		};
 	} else {
 		// Par défaut on retourne les communes les plus peuplées
 		return {
-			body: indexedData
+			body: data
 				.sort((a, b) => b.population - a.population)
 				.slice(0, 10)
 				.map(pick)
