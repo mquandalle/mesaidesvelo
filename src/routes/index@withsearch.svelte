2f69973ff2d89bf87cdc495c1357dc9fade4d60c
Maxime Quandalle <maxime@mesaidesvelo.fr>
2022-05-16 19:48:38 +0200

    Ajout de suggestions

--- src/routes/index@withsearch.svelte
+++ src/routes/index@withsearch.svelte
@@ -2,6 +2,8 @@
 	import { localisation } from '$lib/stores';
 
 	localisation.set(null);
+
+	const biggestCities = ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Nantes'];
 </script>
 
 <svelte:head>
@@ -11,3 +13,15 @@
 		content="Découvrez l’ensemble des aides à l’achat ou la localisation de vélo proposées par votre ville, département, région et par l’État. Simple, rapide et gratuit."
 	/>
 </svelte:head>
+
+<p class="mt-6 w-full max-w-screen-md m-auto text-gray-500 text-right">
+	Essayez avec
+
+	{#each biggestCities as city}
+		<a
+			href="/ville/{city.toLowerCase()}"
+			class="inline-block mb-2 border border-current rounded-xl text-green-500 px-3 py-1 mx-1 hover:(bg-green-500 text-white)"
+			>{city}</a
+		>
+	{/each}
+</p>
