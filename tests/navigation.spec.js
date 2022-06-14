bd6e275bdcd296e97c86078afa7179c0bb89eb3f
Cugniere <16650011+Cugniere@users.noreply.github.com>
2022-06-14 11:06:07 +0200

    Corrige les tests unitaires suite à la fin des aides vélos de la ville de Strasbourg

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -6,9 +6,9 @@ test('Navigation scenario', async ({ page }) => {
 	await page.goto(baseUrl);
 
 	await page.click('[placeholder*="Commune"]');
-	await page.fill('[placeholder*="Commune"]', 'stra');
+	await page.fill('[placeholder*="Commune"]', 'toulou');
 	await page.click('.autocomplete-list-item:first-child');
-	await expect(page).toHaveURL(baseUrl + '/ville/strasbourg');
+	await expect(page).toHaveURL(baseUrl + '/ville/toulouse');
 
 	// Hide evaporate animation
 	await page.addStyleTag({
@@ -17,12 +17,12 @@ test('Navigation scenario', async ({ page }) => {
 
 	await page.click('text=Achat d’un vélo électrique');
 	const totalAides = page.locator('text=Total des aides >> ..');
-	await expect(totalAides).toHaveText('Total des aides 900 €', { useInnerText: true });
+	await expect(totalAides).toHaveText('Total des aides 700 €', { useInnerText: true });
 
-	await page.click('label:last-child');
+	await page.click('label:nth-child(2)');
 	await expect(totalAides).toHaveText('Total des aides 500 €', { useInnerText: true });
 
-	await page.fill('input:below(label:text("Prix du vélo"))', '100');
+	await page.fill('input:below(label:text("Prix du vélo"))', '300');
 	await expect(totalAides).toHaveText('Total des aides 250 €', { useInnerText: true });
 
 	await page.goBack();
