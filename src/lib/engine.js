0127aa0bda2bca0b6c1ecc0df7308a02cbddea29
Maxime Quandalle <maxime@mesaidesvelo.fr>
2022-08-22 16:29:31 +0200

    Corrige l'affichage de l'étiquette conditon de ressources
    Fixes #178

--- src/lib/engine.js
+++ src/lib/engine.js
@@ -1,4 +1,3 @@
-import { page } from '$app/stores';
 import aides from '$lib/../aides.yaml';
 import Publicodes from 'publicodes';
 import { derived } from 'svelte/store';
@@ -6,18 +5,10 @@ import { publicodeSituation } from './stores';
 
 export const engine = new Publicodes(aides);
 
-export const getCurrentBikeEngine = derived(
-	[publicodeSituation, page],
-	([$publicodeSituation, $page]) =>
-		() => {
-			const veloCat = $page.url.searchParams.get('velo') ?? '';
-			engine.setSituation({
-				...$publicodeSituation,
-				'vélo . type': `'${veloCat}'`
-			});
-			return engine;
-		}
-);
+export const getCurrentBikeEngine = derived([publicodeSituation], ([$publicodeSituation]) => () => {
+	engine.setSituation($publicodeSituation);
+	return engine;
+});
 
 export const optionalEvaluate = (expression) => {
 	if (typeof expression === 'string') {
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,4 @@
+import { page } from '$app/stores';
 import { derived, writable } from 'svelte/store';
 
 export const localisation = writable(null);
@@ -18,10 +19,15 @@ export const localisationSituation = derived([localisation], ([$localisation]) =
 );
 
 export const publicodeSituation = derived(
-	[localisationSituation, answers, revenuFiscal],
-	([$localisationSituation, $answers, $revenuFiscal]) => ({
-		...$localisationSituation,
-		...Object.fromEntries(Object.entries($answers).filter(([, val]) => val)),
-		...($revenuFiscal ? { 'revenu fiscal de référence': `${$revenuFiscal} €/mois` } : {})
-	})
+	[localisationSituation, answers, revenuFiscal, page],
+	([$localisationSituation, $answers, $revenuFiscal, $page]) => {
+		// hacky
+		const veloCat = $page.url.searchParams.get('velo') ?? '';
+		return {
+			...$localisationSituation,
+			...Object.fromEntries(Object.entries($answers).filter(([, val]) => val)),
+			...(veloCat ? { 'vélo . type': `'${veloCat}'` } : {}),
+			...($revenuFiscal ? { 'revenu fiscal de référence': `${$revenuFiscal} €/mois` } : {})
+		};
+	}
 );
