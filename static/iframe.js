bba1e4fe2fc5411892b1ca18aedd3375e33d0042
Maxime Quandalle <maxime@mesaidesvelo.fr>
2022-08-28 13:17:23 +0200

    Réduit la taille de iframe.js
    (à la main, le but est que le script reste lisible)

--- static/iframe.js
+++ static/iframe.js
@@ -1,26 +1,14 @@
-/**
- * The goal of this script is to create an iframe and resize it automatically to
- * fit the content inside using iframe postMessage communication between the
- * parent script and the iframe.
- */
+// This script creates an iframe and resizes it automatically to fit its inner content
+{
+	let i = document.createElement('iframe'), e = (l) => encodeURIComponent(location[l]);
+	i.src = `https://mesaidesvelo.fr/?iframe=1&utm_medium=iframe&utm_source=${e('host')}&utm_content=${e('href')}`;
+	i.style.width = '100%';
+	i.style.height = '500px';
+	i.style.border = '0';
+	i.scrolling = 'no';
+	document.currentScript.after(i);
 
-(function () {
-	const iframeElement = document.createElement('iframe');
-	iframeElement.src =
-		'https://mesaidesvelo.fr/?iframe=1&utm_medium=iframe&utm_source=' +
-		encodeURIComponent(window.location.host) +
-		'&utm_content=' +
-		encodeURIComponent(window.location.href);
-	iframeElement.style.width = '100%';
-	iframeElement.style.height = '500px';
-	iframeElement.style.border = '0';
-	iframeElement.scrolling = 'no';
-
-	document.currentScript.after(iframeElement);
-
-	window.addEventListener('message', function (evt) {
-		if (evt.data.kind === 'resize-height') {
-			iframeElement.style.height = evt.data.value + 'px';
-		}
-	});
-})();
+	addEventListener('message', (e) =>
+		e.data.kind === 'resize-height' ? (i.style.height = e.data.value + 'px') : null
+	);
+}
