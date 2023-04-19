9dd915d5d3767cd16e8a538d84d5449dd87ee309
Maxime Quandalle <maxime@mesaidesvelo.fr>
2023-04-19 16:27:22 +0200

    Fix lint

--- src/lib/components/BackButtonAides.svelte
+++ src/lib/components/BackButtonAides.svelte
@@ -4,7 +4,7 @@
 
 {#if $localisation}
 	<a
-		class="inline-block text-gray-500 text-md 
+		class="inline-block text-gray-500 text-md
 cursor-pointer
 hover:text-green-700 transform transition hover:-translate-x-1"
 		data-sveltekit-noscroll
