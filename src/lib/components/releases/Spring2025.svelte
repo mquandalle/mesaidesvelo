cee8293688afb68515c1fa484f488dfe015c5034
Emile Rolley <emile.rolley@tuta.io>
2025-10-07 18:17:35 +0200

    feat: refactor releases + add an historic

---
+++ src/lib/components/releases/Spring2025.svelte
@@ -0,0 +1,35 @@
+<p class="text-sm text-gray-800 mt-2 italic">
+	Ces derniers mois, plusieurs améliorations ont été apportées à <span class="italic"
+		>Mes Aides Vélo</span
+	>
+	par les équipes de
+	<a href="https://beta.gouv.fr" target="_blank" class="text-green-600 hover:underline"
+		>beta.gouv.fr</a
+	> qui ont repris le travail sur la mise à jour des aides. Voici les principales nouveautés :
+</p>
+<ul class="mt-4">
+	<li class="text-sm text-gray-800 mt-2">
+		🚀 <strong>Mise à jour des aides</strong> - les aides sont régulièrement mises à jour. Dernière
+		suppression majeure :
+		<a
+			href="https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000050690951"
+			target="_blank"
+			class="text-green-600 hover:underline">le bonus écologique vélo et la prime à la conversion</a
+		>.
+	</li>
+	<li class="text-sm text-gray-800 mt-2">
+		🦽 <strong>Nouveau type de vélo</strong> - les vélos adaptés pour les personnes à mobilité réduite
+		sont désormais pris en compte.
+	</li>
+	<li class="text-sm text-gray-800 mt-2">
+		🚲 <strong>Vélo d'occasion</strong> - il est désormais possible de choisir les aides en fonction
+		de l'état du vélo (neuf ou occasion).
+	</li>
+</ul>
+<p class="text-sm text-green-600 mt-4">
+	<a
+		href="https://github.com/betagouv/publicodes-aides-velo/releases"
+		target="_blank"
+		class="hover:underline">→ En savoir plus</a
+	>
+</p>
