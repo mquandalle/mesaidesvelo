a987c07103db43d9bde7e61b0bfb46b4c9ecdbd4
Emile Rolley <emile.rolley@tuta.io>
2026-02-13 15:19:24 +0100

    fix: clean ExplanationText

--- src/routes/(front)/(search)/ville/[slug]/ExplanationsText.svelte
+++ src/routes/(front)/(search)/ville/[slug]/ExplanationsText.svelte
@@ -66,15 +66,6 @@
 				ville de {$page.data.ville.nom}. En effet ni la ville, ni le département, ni la région ne
 				proposent d’aide.
 			</p>
-			<p>
-				Si vous y êtes éligible vous pourrez toutefois bénéficier de l’aide nationale accordée par
-				l’État : <strong>le bonus écologique</strong>.
-			</p>
-			<p>
-				Le tableau ci-dessus indique le montant maximal de l’aide de l’État pour chaque type de
-				vélo. Cliquez sur une catégorie pour accéder au calculateur détaillé et personnalisez le
-				résultat en quelques clics, en fonction de vos revenus et du prix du vélo.
-			</p>
 		{/if}
 
 		<h2>Le bonus écologique de l’État</h2>
@@ -94,93 +85,6 @@
 				>.
 			</span>
 		</div>
-
-		<!-- <p> -->
-		<!-- 	Pour bénéficier du bonus écologique de l’État vous devez remplir les conditions suivantes : -->
-		<!-- </p> -->
-		<!-- <ul> -->
-		<!-- 	<li>Être majeur et domicilié en France</li> -->
-		<!-- 	<li> -->
-		<!-- 		Avoir un revenu fiscal de référence par part l'année précédant l'achat du vélo inférieur ou -->
-		<!-- 		égal à 15 400 € -->
-		<!-- 	</li> -->
-		<!-- </ul> -->
-		<!-- <p> -->
-		<!-- 	L’aide est de <strong>40% du prix du vélo</strong>, et elle est plafonnée en fonction du type -->
-		<!-- 	de vélo. Par ailleurs les ménages les plus modestes bénéficient de plafonds plus élevés ainsi -->
-		<!-- 	que d’une aide spécifique pour les vélos mécaniques non électriques. -->
-		<!-- </p> -->
-		<!---->
-		<!-- <table> -->
-		<!-- 	<thead> -->
-		<!-- 		<tr> -->
-		<!-- 			<th rowspan="2">Type de vélo</th> -->
-		<!-- 			<th colspan="2" class="text-center">Plafond de l'aide</th> -->
-		<!-- 		</tr> -->
-		<!-- 		<tr> -->
-		<!-- 			<th>Revenu inférieur à 7&nbsp;100&nbsp;€/an ou personne en situation de handicap</th> -->
-		<!-- 			<th>Revenu inférieur à 15&nbsp;400&nbsp;€/an</th> -->
-		<!-- 		</tr> -->
-		<!-- 	</thead> -->
-		<!-- 	<tbody> -->
-		<!-- 		<tr> -->
-		<!-- 			<td>Mécanique simple</td> -->
-		<!-- 			<td>150 €</td> -->
-		<!-- 			<td>0 €</td> -->
-		<!-- 		</tr> -->
-		<!-- 		<tr> -->
-		<!-- 			<td>Vélo électrique</td> -->
-		<!-- 			<td>400 €</td> -->
-		<!-- 			<td>300 €</td> -->
-		<!-- 		</tr> -->
-		<!-- 		<tr> -->
-		<!-- 			<td>Vélo cargo (avec assistance électrique ou non)</td> -->
-		<!-- 			<td>2 000 €</td> -->
-		<!-- 			<td>1 000 €</td> -->
-		<!-- 		</tr> -->
-		<!-- 		<tr> -->
-		<!-- 			<td>Vélo pliant (avec assistance électrique ou non)</td> -->
-		<!-- 			<td>2 000 €</td> -->
-		<!-- 			<td>1 000 €</td> -->
-		<!-- 		</tr> -->
-		<!-- 		<tr> -->
-		<!-- 			<td>Vélo adapté (avec assistance électrique ou non)</td> -->
-		<!-- 			<td>2 000 €</td> -->
-		<!-- 			<td>1 000 €</td> -->
-		<!-- 		</tr> -->
-		<!-- 	</tbody> -->
-		<!-- </table> -->
-		<!-- <p> -->
-		<!-- 	La demande se fait en ligne au plus tard dans les <strong -->
-		<!-- 		>6 mois suivant l’achat du vélo</strong -->
-		<!-- 	>. -->
-		<!-- </p> -->
-		<!---->
-		<!-- <p> -->
-		<!-- 	En plus du bonus écologique, l’État propose aussi la <a href="/prime-a-la-conversion" -->
-		<!-- 		>prime à la conversion</a -->
-		<!-- 	> qui est une aide supplémentaire accordée en cas de remise à la case d’une vielle voiture essence -->
-		<!-- 	ou diesel. -->
-		<!-- </p> -->
-		<!---->
-		<!-- <h2>Quelles sont les pièces justificatives à fournir ?</h2> -->
-		<!-- <p> -->
-		<!-- 	Les documents demandés peuvent varier selon le type de vélo et l’organisme qui accorde l’aide. -->
-		<!-- 	Dans la majorité des cas vous aurez besoin de -->
-		<!-- </p> -->
-		<!-- <ul> -->
-		<!-- 	<li> -->
-		<!-- 		copie de la facture d'achat acquittée avec le nom, le prénom et l'adresse du bénéficiaire et -->
-		<!-- 		la mention du type de vélo acheté -->
-		<!-- 	</li> -->
-		<!-- 	<li>un justificatif de domicile récent</li> -->
-		<!-- 	<li> -->
-		<!-- 		un justificatif de revenu (dernier avis d’imposition) si l’aide est sous conditions de -->
-		<!-- 		ressources -->
-		<!-- 	</li> -->
-		<!-- 	<li>un RIB pour reçevoir l’argent</li> -->
-		<!-- 	<li>pour les vélos à assiance électrique : la copie du certificat d’homologation du vélo</li> -->
-		<!-- </ul> -->
 	{/if}
 
 	<!-- {#if $page.data.ville.zfe} -->
