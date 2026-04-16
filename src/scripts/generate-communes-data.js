d6618f7c783f91312519275c5de4d262d66785a8
Clément <clem.auger@hotmail.fr>
2026-04-16 16:23:55 +0200

    feat: consume `aides-collectivities` from model properly

--- src/scripts/generate-communes-data.js
+++ src/scripts/generate-communes-data.js
@@ -3,17 +3,35 @@
  *
  * Script to generate a static json file with communes data enriched with EPCI.
  * This is used by other scripts to map collectivities to communes.
- * The slug field is not included here - that's handled by the client.
  */
 
 import { writeJsonData } from './writeData.js';
 
 import communes from '@etalab/decoupage-administratif/data/communes.json' with { type: 'json' };
 import epci from '@etalab/decoupage-administratif/data/epci.json' with { type: 'json' };
+import { slugify } from '../lib/utils.js';
+
+// We add slug to communes. If there are multiple communes with the same name, we add the department code to the slug.
+
+const duplicateCommunesNames = communes
+  .map(({ nom }) => slugify(nom))
+  .sort()
+  .reduce((acc, cur, i, arr) => {
+    if (cur === arr[i - 1] && cur !== acc[acc.length - 1]) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+
+const villesAvecArrondissements = {
+  Paris: "75000",
+  Marseille: "13000",
+  Lyon: "69000",
+};
 
 // Create a map of communes to their EPCI
 const communesInEpci = Object.fromEntries(
-	epci.flatMap(({ nom, membres }) => membres.map(({ code }) => [code, nom])),
+  epci.flatMap(({ nom, membres }) => membres.map(({ code }) => [code, nom])),
 );
 
 // Extra data for Monaco and Luxembourg
@@ -40,27 +58,38 @@ const extraData = [
 
 // Transform communes data
 const data = [
-	...communes
-		.filter((c) => c.type === 'commune-actuelle' && c.codesPostaux && c.population)
-		.map((c) => {
-			const uniq = (l) => [...new Set(l)];
-			const countTrailingZeros = (x) => x.toString().match(/0+$/)?.[0].length ?? 0;
+  ...communes
+    .filter(
+      (c) => c.type === "commune-actuelle" && c.codesPostaux && c.population,
+    )
+    .map((c) => {
+      if (villesAvecArrondissements[c.nom]) {
+        c.codesPostaux.push(villesAvecArrondissements[c.nom]);
+      }
+      const uniq = (l) => [...new Set(l)];
+      const countTrailingZeros = (x) => x.toString().match(/0+$/)?.[0].length ?? 0;
 
-			return {
-				code: c.code,
-				nom: c.nom,
-				departement: c.departement,
-				region: c.region,
-				population: c.population,
-				zfe: false,
-				...(communesInEpci[c.code] ? { epci: communesInEpci[c.code] } : {}),
-				codesPostaux: uniq(c.codesPostaux).sort(
-					(a, b) => countTrailingZeros(b) - countTrailingZeros(a),
-				),
-			};
-		}),
-	...extraData,
-];
+      return {
+        code: c.code,
+        nom: c.nom,
+        departement: c.departement,
+        region: c.region,
+        population: c.population,
+        zfe: false,
+        ...(communesInEpci[c.code] ? { epci: communesInEpci[c.code] } : {}),
+        codesPostaux: uniq(c.codesPostaux).sort(
+          (a, b) => countTrailingZeros(b) - countTrailingZeros(a),
+        ),
+      };
+    }),
+  ...extraData,
+].map((c) => ({
+  ...c,
+  slug: slugify(c.nom)
+    + (duplicateCommunesNames.includes(slugify(c.nom))
+      ? `-${c.departement ?? c.code.slice(0, 2)}`
+      : ""),
+}));
 
 writeJsonData('communes.json', data);
 
